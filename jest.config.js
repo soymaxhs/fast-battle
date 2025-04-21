4c63979288947b38aa78f4f9c350c9601ec58965
soymaxhs <max_heredia95@hotmail.com>
2025-04-20 22:43:32 -0600

    chore: add commitlint and jest configuration files

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
