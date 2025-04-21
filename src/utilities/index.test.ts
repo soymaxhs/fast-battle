470dfb375c1271157f64ba4652b00df110474a11
soymaxhs <max_heredia95@hotmail.com>
2025-04-21 09:59:10 -0600

    feat(utilities): added getPlayerDiceRolls function

--- src/utilities/index.test.ts
+++ src/utilities/index.test.ts
@@ -1,4 +1,4 @@
-import { getRandomDiceRoll } from "@/utilities";
+import { getPlayerDiceRolls, getRandomDiceRoll } from "@/utilities";
 
 describe('getRandomDiceRoll', () => {
   it('should returns a number', () => {
@@ -15,5 +15,27 @@ describe('getRandomDiceRoll', () => {
     const result = getRandomDiceRoll();
     expect(result).toBeGreaterThanOrEqual(1);
     expect(result).toBeLessThanOrEqual(6);
+  });
+});
+
+describe('getPlayerDiceRolls', () => {
+  it('should return an array of numbers', () => {
+    const result = getPlayerDiceRolls();
+    expect(result.every(num => typeof num === 'number')).toBe(true);
+  });
+
+  it('should return an array of integers', () => {
+    const result = getPlayerDiceRolls();
+    expect(result.every(num => Number.isInteger(num))).toBe(true);
+  });
+
+  it('should return an array of numbers between 1 and 6', () => {
+    const result = getPlayerDiceRolls();
+    expect(result.every(num => num >= 1 && num <= 6)).toBe(true);
+  });
+
+  it('should return an array of length 5', () => {
+    const result = getPlayerDiceRolls();
+    expect(result.length).toBe(5);
   });
 });
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -14,3 +14,23 @@
 export function getRandomDiceRoll(): number {
   return Math.floor(Math.random() * 6) + 1;
 }
+
+/**
+ * Generates an array of five simulated dice rolls.
+ *
+ * @summary Simulates a player's turn by rolling five 6-sided dice.
+ * @description This function creates an array with five values, each representing
+ * a roll of a standard 6-sided dice using `getRandomDiceRoll()`. The result can
+ * be used to represent a player's round in a dice-based game.
+ *
+ * @example
+ * const round = getPlayerDiceRolls();
+ * console.log(round); // Output: [3, 6, 1, 4, 2]
+ *
+ * @returns {number[]} An array of five integers between 1 and 6.
+ */
+export function getPlayerDiceRolls(): Array<number> {
+  const playerRound: Array<number> = Array.from({ length: 5 });
+  return playerRound.map(() => getRandomDiceRoll());
+
+}
